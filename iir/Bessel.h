cf6860e5d4d83bbc87e71deed11f9d63fe73d1ed
Yash-V-Tiwari <yashvtiwari04@gmail.com>
2025-09-26 12:53:56 -0500

    bessel

---
+++ iir/Bessel.h
@@ -0,0 +1,25 @@
+#ifndef IIR1_BESSEL_H
+#define IIR1_BESSEL_H
+
+#include "Common.h"
+#include "Cascade.h"
+#include "PoleFilter.h"
+#include "State.h"
+
+namespace Iir {
+
+/**
+ * Filters with Bessel response characteristics. 
+ *  
+ **/
+namespace Bessel
+{
+    
+
+
+    
+} 
+
+}
+
+#endif
